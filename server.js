78a5c73b540f0b454ade955fced758d3840d278a
ganesh-basavoju <basavojuganesh@gmail.com>
2025-04-30 00:33:01 +0530

    dep 1

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use("/api/orders",orderRoutes)
 app.use("/api/payments",paymentRoutes)
 
 
-const port = 5000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:`);
 });
