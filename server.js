24ac664b0f990897d82d655d33edb00f44c3f003
ganesh-basavoju <basavojuganesh@gmail.com>
2025-04-30 01:19:55 +0530

    dep 1

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ const orderRoutes = require('./routes/orderRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://www.zendorr.com",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 require('dotenv').config();
 
 mongoose.connect("mongodb+srv://basavojuganesh:ganeshbasavoju@ecommerce.zx0m8xj.mongodb.net/?retryWrites=true&w=majority&appName=Ecommerce");
