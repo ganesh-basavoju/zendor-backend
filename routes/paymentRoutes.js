0048de898f244c4ffbd78349efc52d3b32452139
lalith <lalithdev123@gmail.com>
2025-05-30 07:57:35 +0530

    razorpay done

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { verifyPayment } = require("../controllers/paymentController");
+const { verifyPayment, createOrder } = require("../controllers/paymentController");
 const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/verify-payment",authMiddleware, verifyPayment )
+router.post('/create-order', authMiddleware,createOrder);
   
 
 
