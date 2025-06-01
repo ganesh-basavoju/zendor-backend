07089724e5f6875d428cbfcb776043268ceacc5c
ganesh-basavoju <basavojuganesh@gmail.com>
2025-06-02 00:25:46 +0530

    spelling

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://www.zendor.com",
+    origin: "https://www.zendorr.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
