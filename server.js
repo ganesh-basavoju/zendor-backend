8c7fc10f43a61eb3ecca6698f61627ae88ff8a2a
ganesh-basavoju <basavojuganesh@gmail.com>
2025-06-01 23:42:06 +0530

    completed zendor

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://www.zendor.com", "http://localhost:3000"],
+    origin: "https://www.zendor.com",// "http://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
