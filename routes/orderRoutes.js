a98a7c605460afda9e011bfa7bc2eaeb52d7cdbc
ganesh-basavoju <basavojuganesh@gmail.com>
2025-06-01 20:28:24 +0530

    hello

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -18,7 +18,7 @@ router.post("/create-order", authMiddleware, createOrder);
 router.get("/getAllOrders", authMiddleware, getAllOrders);
 router.get("/get-orders", authMiddleware, getOrders);
 router.get("/getOrderDetails/:orderId", authMiddleware, getOrderDetails);
-router.patch("/updateOrderStatus/:orderId", authMiddleware, updateOrderStatus);
+router.put("/updateOrderStatus/:orderId", authMiddleware, updateOrderStatus);
 router.put("/update/:id", authMiddleware, updateOrder);
 
 module.exports = router;
